f5d627e5d2f79875ec300b4bcb7be1ae8a53ba41
William Chow <williamchowhk@gmail.com>
2025-02-04 23:06:35 -0600

    add parent node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -31,4 +31,18 @@ class LeafNode(HTMLNode):
         props = self.props_to_html()
         return f"<{self.tag}{props}>{self.value}</{self.tag}>"
 
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("tag cannot be empty")
+        if self.children is None:
+            raise ValueError("children is missing")
+        html = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children:
+            html += child.to_html()
+        html += f"</{self.tag}>"
+        return html
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode,LeafNode
+from htmlnode import HTMLNode,LeafNode,ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html_props(self):
@@ -17,7 +17,31 @@ class TestHTMLNode(unittest.TestCase):
         node1 = LeafNode("a", "Click me!", {"href":"https://www.google.com"})
         node2 = LeafNode("p", "This is a paragraph of text.")
         self.assertEqual( node1.to_html(), "<a href=\"https://www.google.com\">Click me!</a>" )
-        self.assertEqual( node2.to_html(), "<p>This is a paragraph of text.</p>" );
+        self.assertEqual( node2.to_html(), "<p>This is a paragraph of text.</p>" )
+
+    def test_parent_node(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ])
+        self.assertEqual( node.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>" )
+
+    def test_parent_node_2(self):
+        node = ParentNode("p", [
+                ParentNode("p", [
+                 ParentNode("p", [
+                  LeafNode("b", "Bold text"),
+                  LeafNode(None, "Normal text"),
+                  LeafNode("i", "italic text"),
+                  LeafNode(None, "Normal text")
+                 ], {'class':'test1'} )
+                ], {'class':'test2'} )
+               ] )
+        self.assertEqual( node.to_html(), "<p><p class=\"test2\"><p class=\"test1\"><b>Bold text</b>Normal text<i>italic text</i>Normal text</p></p></p>" )
 
 if __name__ == "__main__":
     unittest.main()
